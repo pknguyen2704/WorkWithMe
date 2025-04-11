b3160baaa53bdc2ff439b8ea0362b8d079866d0d
pknguyen2704 <pknguyen2704@gmail.com>
2025-04-12 01:26:05 +0700

    test call API

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,23 @@ import Box from '@mui/material/Box'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mockData'
+// import { mockData } from '~/apis/mockData'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
+
 function Board() {
+    const [board, setBoard] = useState(null)
+    useEffect(() => {
+        const boardID = '67f94fdfe2b38fc65e229ce5'
+        fetchBoardDetailsAPI(boardID).then((board) => {
+            setBoard(board)
+        })
+    }, [])
     return (
         <Container disableGutters maxWidth={false} sx={{height: '100vh'}}>
             <AppBar></AppBar>
-            <BoardBar board={mockData?.board}></BoardBar>
-            <BoardContent board={mockData?.board}></BoardContent>
+            <BoardBar board={board}></BoardBar>
+            <BoardContent board={board}></BoardContent>
         </Container>
     );
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const API_ROOT = 'http://localhost'
+export const API_ROOT = 'http://localhost:3000'
